a4f36ae63468ecad20a92b3458967eac542ef304
MarcosMoises <marcosmoises159@gmail.com>
2016-10-16 12:56:54 -0200

    adicionado telas do gerente, com nav e side bar

--- js/padariaScripts.js
+++
@@ -1,8 +0,0 @@
-function touchRosca(){
-	if(document.getElementById("rosquinha").getAttribute('src') == "imgs/rosquinhaSmile.png"){
-		document.getElementById("rosquinha").src = "imgs/rosquinhaPiscada.png";
-	}			
-}
-$( document ).ready(function(){
-	$(".button-collapse").sideNav();
-});
